2362dd1aaabdde303eb18ed19df2cdbd2941ac0c
xuyq <xuyq@163.com>
2024-03-18 00:34:27 +0800

    add source ros humbe setup

--- colcon_build.sh
+++ colcon_build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-colcon build --merge-install --cmake-args -DBUILD_TESTING=0 -DCMAKE_TOOLCHAIN_FILE=$(PWD)/cross-compile-configure.cmake
+source /opt/ros/humbel/setup.bash
+colcon build --merge-install --cmake-args -DBUILD_TESTING=0 -DCMAKE_TOOLCHAIN_FILE=${PWD}/cross-compile-configure.cmake
